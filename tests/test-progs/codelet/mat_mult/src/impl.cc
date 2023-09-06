8386f194970d34e062c90a5069ecb1e5390bde71
Dawson Fox <dfox@josemanldesk.hsd1.il.comcast.net>
2023-09-06 10:41:21 -0500

    adding error checking to matrix multiplication; results are now verified

--- tests/test-progs/codelet/mat_mult/src/impl.cc
+++ tests/test-progs/codelet/mat_mult/src/impl.cc
@@ -22,18 +22,24 @@ void scm_init() {
     // this is used to offer the users a way to do setup for a benchmark and initialize
     // the memory space that memory codelets will load from
     printf("initializing data space starting at %p\n", SCM_MEMORY_BASE_PTR);
-    //long unsigned mmap_ret = (long unsigned) mmap((void*)SCM_MEMORY, SCM_MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE, -1, 0);
-    // removed MAP_FIXED_NOREPLACE because this stack variable is probably not on a page boundary
-    //long unsigned mmap_ret = (long unsigned) mmap((void*)SCM_MEMORY_BASE_PTR, SCM_MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    //printf("SCM init has mmap return value %lx\n", mmap_ret);
     double * mem_space = (double *) SCM_MEMORY_BASE_PTR;
+    // initialize the data space with two matrices TILE_DIM x TILE_DIM matrices
     for (int i=0; i<TILE_DIM*TILE_DIM*2; i++) {
       mem_space[i] = (double) i;
-      printf("%lf ", mem_space[i]);
-      fflush(NULL);
     }
-    printf("data space initialized from %p to %p\n", mem_space, &(mem_space[TILE_DIM*TILE_DIM-1]));
-    fflush(NULL);
+    // doing a matrix multiplication in the 4th matrix slot of the mem space to for error checking at the end
+    //printf("result matrix will be at %p\n", &(mem_space[TILE_DIM*TILE_DIM*2]));
+    //printf("building check matrix at %p\n", &(mem_space[TILE_DIM*TILE_DIM*3]));
+    // 4th matrix = 1st matrix * 2nd matrix
+    for (int i=0; i<TILE_DIM; i++) {
+      for (int j=0; j<TILE_DIM; j++) {
+        for (int k=0; k<TILE_DIM; k++) {
+          mem_space[TILE_DIM*TILE_DIM*3+i*TILE_DIM+j] += mem_space[i*TILE_DIM+k] * mem_space[TILE_DIM*TILE_DIM + k*TILE_DIM+j];
+        }
+      }
+    }
+    //printf("data space initialized from %p to %p\n", mem_space, &(mem_space[TILE_DIM*TILE_DIM-1]));
+    //fflush(NULL);
 }
 
 void loadSqTile(void * dest, void * src1, void * src2) {
@@ -43,20 +49,12 @@ void loadSqTile(void * dest, void * src1, void * src2) {
   uint64_t ldistance = (uint64_t) src2; //change for immediate value
 
   ldistance *= sizeof(double);
-  printf("address reg: 0x%lx; ldistance: 0x%lx; scm memory: %p; first element: %lf\n", address_reg, ldistance, SCM_MEMORY_BASE_PTR, ((double *)SCM_MEMORY_BASE_PTR)[2]);
-  fflush(NULL);
+  //printf("address reg: 0x%lx; ldistance: 0x%lx; scm memory: %p; first element: %lf\n", address_reg, ldistance, SCM_MEMORY_BASE_PTR, ((double *)SCM_MEMORY_BASE_PTR)[2]);
+  //fflush(NULL);
   for (uint64_t i=0; i<TILE_DIM; i++) {
     double *addressStart = (double *) (SCM_MEMORY_BASE_PTR + address_reg + ldistance * i); // Address L2 memory to a pointer of the runtime
-    printf("loading tile from %p to %p\n", addressStart, destReg+TILE_DIM*i);
+    //printf("loading tile from %p to %p\n", addressStart, destReg+TILE_DIM*i);
     memcpy(destReg+TILE_DIM*i, addressStart, TILE_DIM * sizeof(double));
-  }
-  for (int i=0; i<TILE_DIM; i=i+1){
-    for (int j=0; j<TILE_DIM; j=j+1){
-      printf("%lf ", destReg[i*TILE_DIM+j]);
-      fflush(NULL);
-    }
-    printf("\n");
-    fflush(NULL);
   }
 }
 
@@ -70,32 +68,35 @@ void matMult(void * dest, void * src1, void * src2) {
       for (int k=0; k<TILE_DIM; k=k+1) {
         C[i*TILE_DIM + j]+=((A[i*TILE_DIM + k])*(B[k*TILE_DIM + j]));
       }
-      printf("%lf ", C[i*TILE_DIM + j]);
-      fflush(NULL);
-    //printf("col %d done\n", j);
     }
-    //printf("row %d done\n", i);
   }
 }
 
 void storeSqTile(void * dest, void * src1, void * src2) {
   // Obtaining the parameters
-  //uint8_t* address_reg = (uint8_t *) src1;
   uint64_t address_reg = *((uint64_t *)src1);
   //uint64_t ldistance = *((uint64_t *) src2);
   uint64_t ldistance = (uint64_t) src2;
   double *sourceReg = (double *) dest;
-  for (int i=0; i<TILE_DIM; i=i+1){
-    for (int j=0; j<TILE_DIM; j=j+1){
-      printf("%lf ", sourceReg[i*TILE_DIM+j]);
-      fflush(NULL);
-    }
-    printf("\n");
-    fflush(NULL);
-  }
   ldistance *= sizeof(double);
   for (uint64_t i = 0; i < TILE_DIM; i++) {
     double *addressStart = (double *) (SCM_MEMORY_BASE_PTR + address_reg + ldistance * i); // Address L2 memory to a pointer of the runtime
+    //printf("copying back to %p\n", addressStart);
     memcpy(addressStart, sourceReg+TILE_DIM*i, TILE_DIM * sizeof(double));
+  }
+  // after copying, do error checking in the memory space
+  // note that if we change the algorithm, this will always fail because of float inaccuraccies
+  double * result_mat = (double *) (SCM_MEMORY_BASE_PTR + address_reg); //index SCM mem space to where result is store
+  double * check_mat = (double *) (SCM_MEMORY_BASE_PTR + address_reg + TILE_DIM*TILE_DIM*8); //index one matrix size more for the matrix made in init
+  //printf("reading result_mat from %p and check mat from %p\n", result_mat, check_mat);
+  bool correct = true;
+  for (int i=0; i<TILE_DIM*TILE_DIM; i++) {
+    if (result_mat[i] != check_mat[i]) {
+      printf("error: element %d does not match: %lf vs %lf\n", i, result_mat[i], check_mat[i]);
+      correct = false;
+    }
+  }
+  if (correct) {
+    printf("Matrix multiplication completed! No errors detected\n");
   }
 }
